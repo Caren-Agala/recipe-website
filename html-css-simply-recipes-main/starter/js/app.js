6594e21595d31c8fc3d64f91cdea3ab4f827dfd0
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2022-04-07 11:53:19 +0300

    JS changes for changing year in footer

--- html-css-simply-recipes-main/starter/js/app.js
+++ html-css-simply-recipes-main/starter/js/app.js
@@ -15,3 +15,7 @@ navBtnDOM.addEventListener('click',()=>{
   links.classList.toggle('show-links')
 })
 
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
